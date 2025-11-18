609325199901633e0dcab8d5463c1de956152fda
Dave Miller <39599524+dmiller-tibco@users.noreply.github.com>
2025-11-18 17:57:05 -0600

    remove obsolete chart: msg-pulsar-tp (#335)
    * remove obsolete chart: msg-pulsar-tp

--- charts/msg-pulsar-tp/scripts/ops-pulsar-bash-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-# Copyright (c) 2023-2025. Cloud Software Group, Inc.
-# This file is subject to the license terms contained
-# in the license file that is distributed with this file.
-#
-
-/bin/bash
